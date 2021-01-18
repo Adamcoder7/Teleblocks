e85286cce284b85f689f747c0bcfa3742ce9ad7b
Adamcoder7 <77583105+Adamcoder7@users.noreply.github.com>
2021-01-18 08:23:18 +0100

    Add files via upload

---
+++ src/testplugin/Testplugin.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Adam
+ */
+public class Testplugin extends JavaPlugin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    @Override
+public void onEnable() {
+}
+
+@Override
+public void onDisable() {
+}
+    
+}
